406753d026a31e6fdf3edbaa2fd671127da9d6c4
Boaty McBoatFace <bfhensel@gmail.com>
2022-09-23 11:51:58 -0400

    renaming & json stuff

--- yosemite.sh
+++ yosemite.sh
@@ -502,14 +502,14 @@ for BL in $BLACKLISTS; do
     # Blacklisted
     else
         if [ $VERBOSE -ge 1 ]; then
-            if test -z "$PLAIN"; then printf "\r"; fi
+            if test -z "$PLAIN"; then printf ""; fi
             printf "%s%sblacklisted (%s)%s\n" "$RED" "$PREFIX" "$RESPONSE" "$CLEAR";
         elif [ $VERBOSE -ge 0 ]; then
             if test -z "$PLAIN"; then printf "";
-            printf "%s\n";
+            printf "{";
           fi
           # printf "%s%s%s : %s\n" "$RED" "$BL" "$CLEAR" "$RESPONSE";
-           printf "{blacklist:%s$BL}\n"
+           printf "\"blacklist\":%s\"$BL\",\n"
 
         fi
         FAILED=$((FAILED + 1))
@@ -524,7 +524,7 @@ if [ $VERBOSE -ge 0 ]; then
         printf ""
     fi
 #    printf "~hostname:$HOSTNAME,"
-    printf "{ipv4_public:$TARGET}\n"
+    printf "\"ipv4_public\":%s\"$TARGET\"}\n"
 #    printf "Tested:${COUNT},"
 #    printf "Passed:${GREEN}${PASSED}${CLEAR},"
 #    printf "Invalid:${YELLOW}${INVALID}${CLEAR},"
